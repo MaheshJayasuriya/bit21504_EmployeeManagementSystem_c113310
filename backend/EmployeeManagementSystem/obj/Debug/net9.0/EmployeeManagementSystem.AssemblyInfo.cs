7d0f1282575586b70091f6507e42c210db79eaec
mahesh <170492349+MaheshJayasuriya@users.noreply.github.com>
2025-09-29 00:05:19 +0530

    Delete obsolete files not required

--- backend/EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ backend/EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f753d061382523c18871e405d2260a4a490103")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
